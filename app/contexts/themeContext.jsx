7371e119a6aaf987adde702b658cb9855ca8bb34
Bryan Quek <bryanquek00@gmail.com>
2024-02-14 17:12:31 +0800

    Added dark mode caching

--- app/contexts/themeContext.jsx
+++ app/contexts/themeContext.jsx
@@ -1,15 +1,34 @@
 'use client';
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect} from 'react';
 
 export const AppContext = createContext();
 
 export function AppProvider({ children}) 
 {
-    const [darkMode, setDarkMode] = useState(false);
+    const [mount, setMount]= useState(false)
+    const [darkMode, setDarkMode] = useState(() => {
+        if (typeof window !== 'undefined') {
+            return localStorage.getItem('darkMode') === 'true';
+        }
+        return false; // Default mode
+    });
+
+    useEffect(() => {
+        setMount(true); // This will set mount to true when the component mounts
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('darkMode', darkMode);
+        console.log(localStorage.getItem('darkMode'))
+    }, [darkMode]);
 
     return (
+    <>
+        {mount && (
         <AppContext.Provider value={{ darkMode, setDarkMode }}>
             {children}
-        </AppContext.Provider>
+            </AppContext.Provider>
+            )}
+    </>
     );
 }
