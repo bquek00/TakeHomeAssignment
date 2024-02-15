3b1fbc05f941dbb4a8952313945daab28f954846
Bryan Quek <bryanquek00@gmail.com>
2024-02-15 18:56:15 +0800

    Fixed style for themeContext

--- app/contexts/themeContext.jsx
+++ app/contexts/themeContext.jsx
@@ -6,34 +6,38 @@ export const AppContext = createContext();
 
 export function AppProvider({ children}) 
 {
-    const [mount, setMount]= useState(false)
+    const [mount, setMount]= useState(false);
     const [data, setData] = useState([]);
 
-    const [darkMode, setDarkMode] = useState(() => {
-        if (typeof window !== 'undefined') {
+    const [darkMode, setDarkMode] = useState(() => 
+    {
+        if (typeof window !== 'undefined') 
+        {
             return localStorage.getItem('darkMode') === 'true';
         }
         return false; // Default mode
     });
 
-    useEffect(() => {
+    useEffect(() => 
+    {
         setData(jsonData); // Set the imported JSON data into state
-        console.log(jsonData)
+        console.log(jsonData);
         setMount(true); // This will set mount to true when the component mounts
     }, []);
 
-    useEffect(() => {
+    useEffect(() => 
+    {
         localStorage.setItem('darkMode', darkMode);
-        console.log(localStorage.getItem('darkMode'))
+        console.log(localStorage.getItem('darkMode'));
     }, [darkMode]);
 
     return (
-    <>
-        {mount && (
-        <AppContext.Provider value={{ darkMode, setDarkMode, data, setData}}>
-            {children}
-            </AppContext.Provider>
+        <>
+            {mount && (
+                <AppContext.Provider value={{ darkMode, setDarkMode, data, setData}}>
+                    {children}
+                </AppContext.Provider>
             )}
-    </>
+        </>
     );
 }
